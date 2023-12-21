1c4b057d66587d52f432a40bc9d62a8ef229fe4f
ando <ando@maslab.aitech.ac.jp>
2023-12-21 13:36:45 +0900

    Fix rotate speed

--- Assets/Scripts/RotateController.cs
+++ Assets/Scripts/RotateController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotateController : MonoBehaviour
 {
+    [Tooltip("抽選機の回転速度")]
+    public float rotateSpeed = 20.0f;
+
     // ガラポンのオブジェクト
     private GameObject garapon;
     // 回転／停止ボタンのオブジェクト
@@ -11,22 +14,33 @@ public class RotateController : MonoBehaviour
     // ガラポンの回転／停止状態
     private bool isRotate = false;
 
-    // ガラポンの回転スピード
-    public float speed = 1.0f;
-
     private void Start()
     {
-        garapon = GameObject.Find("Garapon");
-        rotateControlButton= GameObject.Find("RotateControlButton");
     }
+
     private void Update()
     {
-        garapon.transform.Rotate(0.0f, 0.0f, isRotate ? -0.2f * speed : 0.0f);
+        // 抽選機のオブジェクトを取得
+        if (garapon == null)
+        {
+            garapon = GameObject.Find("Garapon");
+        }
+        // 抽選機のオブジェクトを取得
+        if (rotateControlButton == null)
+        {
+            rotateControlButton = GameObject.Find("RotateControlButton");
+        }
+        // 回転方向への力
+        Vector3 power = new Vector3(0.0f, 0.0f, isRotate ? -rotateSpeed : 0.0f);
+        // 抽選機の回転
+        garapon.transform.Rotate(power * Time.deltaTime);
     }
 
     public void SwitchRotateState()
     {
+        // 回転／停止の切り替え
         isRotate = !isRotate;
+        // 回転制御ボタンのテキストの変更
         rotateControlButton.GetComponentInChildren<UnityEngine.UI.Text>().text = isRotate ? "とめる" : "まわす";
     }
 }
